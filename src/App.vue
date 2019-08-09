3b82e5dd0b45f205a150f55680552095ceefaa39
Nicks Home Computer <vaughnick@gmail.com>
2019-08-08 22:37:05 -0400

    create todo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
   </div>
 </template>
 
 <script>
 import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateTodo";
 
 export default {
   components: {
-    TodoList
+    TodoList,
+    CreateTodo
   },
-
   data() {
     return {
       todos: [
@@ -37,6 +39,12 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    addTodo(todo) {
+      console.log("created");
+      this.todos.push(todo);
+    }
   }
 };
 </script>
